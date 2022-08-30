0d98ff8c8287ebe3bf8ccda778910230174dc2a9
SiddhiPopade <siddhi182000@gmail.com>
2022-08-30 17:33:33 +0530

    first commit

---
+++ src/main/java/com/example/demo/model/JwtResponse.java
@@ -0,0 +1,30 @@
+package com.example.demo.model;
+
+public class JwtResponse {
+	
+	String token;
+
+	public JwtResponse(String token) {
+		super();
+		this.token = token;
+	}
+
+	public JwtResponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	@Override
+	public String toString() {
+		return "JwtResponse [token=" + token + "]";
+	};
+
+}
